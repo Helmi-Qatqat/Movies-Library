d64fdb3717e916e1cb273b533cc69a2a48ceff97
Helmi-Qatqat <helmiqatqat@gmail.com>
2023-05-03 23:53:49 +0300

    server built and requests are handled

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Movies-Library
+# Movies-Library - 1.0v
+
+**Author Name**: Helmi Qatqat
+
+## WRRC
+![image](./images/WRRC.png)
+
+## Overview
+
+
+## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+1. Build a server that handles the requests of movies.
+2. Have a database for movies data
+3. Use frontend library 'React' to handle the UI.
+
+## Project Features
+<!-- What are the features included in you app -->
+1. User Accout.
+2. Ability to make a favorite list of movies.
+3. Watch Movies in different resolutions.
---
+++ server.js
@@ -0,0 +1,47 @@
+require("dotenv").config()
+const express = require('express');
+const server = express();
+const port = process.env.PORT_KEY
+const cors = require('cors');
+const Data = require('./Movie-Data/data.json')
+
+server.use(cors());
+
+function Movie(title, poster_path, overview) {
+  this.title = title;
+  this.poster_path = poster_path;
+  this.overview = overview
+}
+
+function errorHandler(req, res) {
+  res.status(404).send('Page Not Found')
+}
+
+
+
+server.get("/", (req,res) => {
+  res.status(200).send(new Movie(Data.title, Data.poster_path, Data.overview))
+})
+
+
+
+server.get("/favorite", (req, res) => {
+  res.status(200).send('Welcome to Favorite Page')
+})
+
+server.get('/crash', (req, res) => {
+  throw new Error('Server crash');
+});
+
+server.use((err, req, res, next) => {
+  res.status(500).json({
+    status: 500,
+    responseText: "Sorry, something went wrong"
+  });
+});
+
+server.get("*", errorHandler)
+
+server.listen(port, () => {
+	console.log(`server is listinging on port ${port}`) // Message to be displayed on terminal
+})
