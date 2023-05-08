2a38481acd6ccfeab6571628fff95eff08277318
Helmi-Qatqat <helmiqatqat@gmail.com>
2023-05-09 01:02:10 +0300

    addMovie, getMovies functionalities  are added

---
+++ schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS movies_lists;
+CREATE TABLE IF NOT EXISTS movies_lists (
+  id SERIAL PRIMARY KEY,
+  title varchar(255),
+  release_date varchar(255),
+  poster_path varchar(255),
+  overview varchar(255)
+)
