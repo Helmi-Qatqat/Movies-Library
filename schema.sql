73184249b433541916a08a591d6ac5a06f381f7d
Helmi-Qatqat <helmiqatqat@gmail.com>
2023-05-18 01:59:07 +0300

    schema increase data size

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS movies_lists;
 CREATE TABLE IF NOT EXISTS movies_lists (
-  id varchar(255),
-  title varchar(255),
-  release_date varchar(255),
-  poster_path varchar(255),
-  overview varchar(255),
-  comment varchar(255)
+  id int,
+  title varchar(4096),
+  release_date varchar(4096),
+  poster_path varchar(4096),
+  overview varchar(4096),
+  comment varchar(4096)
 );
